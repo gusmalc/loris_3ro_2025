a8db50e4beb7bba9eb77f76657f36bc7506ad919
Gustavo Malcotti <gustavo@malcotti.com>
2025-06-11 08:18:00 -0300

    1

---
+++ clase_05/scripts.js
@@ -0,0 +1,18 @@
+
+// let edad;
+
+// edad = 10;
+
+// if(edad < 18){
+//     alert("Es menor");
+// }else{
+//     alert("Es mayor");
+// }
+
+
+let num1 = 10;
+let num2 = "20";
+
+let resultado = num1 + num2;
+
+alert(resultado);
