9786a448865b1b53e53909780db29312a1dbce76
Gustavo Malcotti <gustavo@malcotti.com>
2025-09-17 09:33:26 -0300

    1

---
+++ clase_07/scripts.js
@@ -0,0 +1,44 @@
+let nombre = "Ronaldo";
+
+let numero_1 = "Juan ";
+
+let numero_2 = "Lopez";
+
+
+let a = 4;
+let b = 65;
+
+let c = a + b;
+
+
+if(c > 10 && c < 200) {
+    console.log("Es mayor que 10");
+}else{
+    console.log("Es menor o igual que 10");
+}
+
+if(c == 5){
+    ////
+}
+if(c == 8){
+    ////
+}
+
+
+//console.log(numero_1 + numero_2);  
+
+// let entrada = prompt("Ingresa un numero entero");
+
+// let entrada2 = prompt("Ingresa otro numero entero");
+
+// let textoEscrito = prompt("Escribe un texto", "Texto");
+// if(textoEscrito != null){
+//     alert("Has escrito " + textoEscrito);
+// } else {
+//     alert("No has escrito nada");
+
+
+// }
+
+
+// console.log(parseInt(entrada) + parseInt(entrada2));
